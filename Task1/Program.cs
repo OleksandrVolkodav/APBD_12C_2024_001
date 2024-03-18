7d0c5fbd3b90a9fafbd8b76aa82ea358583f6cbb
s28021 <126588178+OleksandrVolkodav@users.noreply.github.com>
2024-03-18 13:31:01 +0100

    changes

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello, Warsaw!");
+
+static double average(int[] arr) {
+    return arr.Average();
+    // int result = 0;
+    // for(int i = 0; i < arr.Length; i++) {
+    //   result += arr[i];
+    // }
+    // return result/arr.Length;
+}
